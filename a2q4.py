12c35821c8e1f2106f8d5edde10ee36604b62726
Harry6542 <ozc189@usak.ca>
2023-06-18 14:51:09 -0600

    def are_community_besties()

--- a2q4.py
+++ a2q4.py
@@ -63,3 +63,25 @@ def leave_community(community: list, name: str) -> None:
                     foe_person["foes"].remove(name)
             community.remove(person)
             break
+def are_community_besties(community: list, name1: str, name2: str) -> bool:
+    """
+    Determine if the two names passed in are within the community and if they are on each
+    other's "friends" list.
+
+    Args:
+        community (list): List of dictionaries to iterate through. Each dictionary must contain the keys
+                          "name", "friends", and "foes". The "name" values should be unique, and the
+                          "friends" and "foes" values must be lists.
+        name1 (str): Name of the first person being examined within the list.
+        name2 (str): Name of the second person being examined within the list.
+
+    Returns:
+        bool: True if both names are within the community and their names are in each other's "friends" list,
+              False otherwise.
+    """
+    for person in community:
+        if person["name"] == name1 and name2 in person["friends"]:
+            for friend in community:
+                if friend["name"] == name2 and name1 in friend["friends"]:
+                    return True
+    return False
